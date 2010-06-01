2ed9998d7f28a2b67b88dfafafc92538ef99c3bd
Colin Edwards <ddrboxman@gmail.com>
2010-06-01 15:04:25 -0400

    Starting refactoring

---
+++ src/com/andrewshu/android/reddit/RedditCommunicator.java
@@ -0,0 +1,16 @@
+package com.andrewshu.android.reddit;
+
+public class RedditCommunicator {
+
+	public void postRequest() {
+		
+	}
+	
+	public void getRequest() {
+		
+	}
+	
+	public void vote() {
+		
+	}
+}
